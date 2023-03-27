00ab7d915326df06214af67fe63aaec612ea5244
3corneredhat <alancabrera10@gmail.com>
2023-03-27 10:35:52 -0500

    chore: learned js if-else

--- script.js
+++ script.js
@@ -13,36 +13,66 @@ btn.addEventListener("click", function () {
   }
 });
 
-/* 'let' is for variables that are able to be updated
-later*/
-let votesInteresting = 23;
-let votesMindblowing = 5;
+// /* 'let' is for variables that are able to be updated
+// later*/
+// let votesInteresting = 23;
+// let votesMindblowing = 5;
 
-/* 'const' are for variables that will remain constant
-or will not be changed later on */
-const text = "Lisbon is the capital of Portugal";
+// /* 'const' are for variables that will remain constant
+// or will not be changed later on */
+// const text = "Lisbon is the capital of Portugal";
 
-votesInteresting = votesInteresting + 1;
-votesInteresting++;
-console.log(votesInteresting);
+// votesInteresting = votesInteresting + 1;
+// votesInteresting++;
+// console.log(votesInteresting);
 
-let totalUpvotes = votesInteresting + votesMindblowing;
+// let totalUpvotes = votesInteresting + votesMindblowing;
 
-console.log("Upvotes:", totalUpvotes);
+// console.log("Upvotes:", totalUpvotes);
 
-let votesFalse = 4;
-const isCorrect = votesFalse < totalUpvotes;
+// let votesFalse = 4;
+// const isCorrect = votesFalse < totalUpvotes;
 
-console.log(isCorrect);
+// console.log(isCorrect);
 
 /*Lecture 36*/
 
 function calcFactAge(year) {
   const currentYear = new Date().getFullYear();
   const age = currentYear - year;
-  return age;
+  if (age >= 0) return age;
+  else return "Impossible year";
 }
 
 const age1 = calcFactAge(2015);
 console.log(age1);
-console.log(calcFactAge(1190));
+console.log(calcFactAge(2052));
+
+let votesInteresting = 20;
+let votesMindblowing = 20;
+
+if (votesInteresting === votesMindblowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (votesInteresting > votesMindblowing) {
+  console.log("Interesting Fact");
+} else if (votesInteresting < votesMindblowing) {
+  console.lof("Mindblowing fact!!");
+}
+
+//falsy 0 , '', null, undefined
+//truthy value: everything else...
+if (votesMindblowing) {
+  console.log("MINDBLOWING FACT!!!");
+} else {
+  console.log("Not so special...");
+}
+
+let votesFalse = 77;
+const totalUpvotes = votesInteresting + votesMindblowing;
+
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true"
+    : "Might be false, check more sources...";
+
+// alert(message);
