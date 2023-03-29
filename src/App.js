f5b274519b9c6e6a831babc018a50762880bf2bc
3corneredhat <alancabrera10@gmail.com>
2023-03-29 09:41:25 -0500

    feat: added react component skeleton

--- src/App.js
+++ src/App.js
@@ -10,14 +10,40 @@ function App() {
   In JSX we cannont use the word 'class' as it is a reserved word.
   JSX expressions must have one parent element.
   React will look in Public folder for items.*/
+  const appTitle = "Today I Learned";
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Today I learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    /* '<>' is called a fragment, we used it because without it
+    we return two objects. This packages the objects into one 
+    without adding unneccessary elements to the html.*/
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+function CategoryFilter() {
+  return <aside> Category filter </aside>;
+}
+function FactList() {
+  return <section>Facts List</section>;
 }
 export default App;
