c3530318131a7f85248b97a118618cec1d7708eb
3corneredhat <alancabrera10@gmail.com>
2023-03-29 11:01:25 -0500

    feat: rendered the list of facts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,51 @@ import "./styles.css";
 and HTML is inserted into it. We are inserting everything
 in the index.html class "root". */
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 //Uppercase letter of a function indicates a component.
 function App() {
   /*The following is not HTML, it is JSX, a syntax that
@@ -44,6 +89,42 @@ function CategoryFilter() {
   return <aside> Category filter </aside>;
 }
 function FactList() {
-  return <section>Facts List</section>;
+  const facts = initialFacts;
+  return (
+    <section>
+      <ul className="facts-list">
+        {
+          //loop over the facts using the map method.
+          facts.map((fact) => (
+            <li key={fact.id} className="fact">
+              <p>
+                {fact.text}
+                <a className="source" href={fact.source} target="_blank">
+                  (Source)
+                </a>
+              </p>
+              {/* We use nested curly braces for the style: 1 set 
+              to indicate Javascript and 1 set to indicate an object. */}
+              <span
+                className="tag"
+                style={{
+                  backgroundColor: CATEGORIES.find(
+                    (cat) => cat.name === fact.category
+                  ).color,
+                }}
+              >
+                {fact.category}
+              </span>
+              <div className="vote-buttons">
+                <button>👍 {fact.votesInteresting}</button>
+                <button>🤯 {fact.votesMindblowing}</button>
+                <button>⛔️ {fact.votesFalse}</button>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </section>
+  );
 }
 export default App;
