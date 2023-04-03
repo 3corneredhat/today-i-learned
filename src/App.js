a1e402d0f2aaf0b1176cd830d496e940efc7e3c1
3corneredhat <alancabrera10@gmail.com>
2023-04-03 09:26:57 -0500

    feat: added loader component and error handling

--- src/App.js
+++ src/App.js
@@ -83,10 +83,23 @@ function App() {
   //for displaying stored facts.
   const [facts, setFacts] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(function () {
     async function getFacts() {
-      const { data: facts, error } = await supabase.from("facts").select("*");
-      setFacts(facts);
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .order("votesInteresting", { ascending: false })
+        .limit(1000);
+
+      console.log(facts);
+      console.log(error);
+
+      if (!error) setFacts(facts);
+      else alert("There was a problem getting data");
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -102,10 +115,18 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} setShowForm={setShowForm} />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <FactList facts={facts} setShowForm={setShowForm} />
+        )}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">Loading...</p>;
 }
 
 function Header({ showForm, setShowForm }) {
